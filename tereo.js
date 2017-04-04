af6896096022a5ea2d395986104460e2e68ec6dc
Alisa Yunusova <alisa.yunusova@gmail.com>
2017-04-04 20:35:31 +1200

    Add js file and make backbone function to listen to button click

---
+++ tereo.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', startGame)
+
+function startGame() {
+
+    var btn = document.getElementById("btn");
+
+    btn.addEventListener("click", function() {
+        alert("hello")
+    });
+
+
+}
