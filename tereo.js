b6ebb88b7a4d9de47082569c07187624403bf8aa
Alisa Yunusova <alisa.yunusova@gmail.com>
2017-04-04 21:30:58 +1200

    Create request to test connection to the API

--- tereo.js
+++ tereo.js
@@ -4,11 +4,21 @@ function startGame() {
 
     var btn = document.getElementById("btn");
 
-    btn.addEventListener("click", function() {
-        alert(englishWord.value)
-    });
+    btn.addEventListener("click", translateWord);
 
-    var englishWord = document.getElementById("englishWord");
+    function translateWord() {
+        //  alert(englishWord.value)
+
+        var englishWord = document.getElementById("englishWord");
+        var ourRequest = new XMLHttpRequest();
+
+        ourRequest.open('GET', 'https://eda-te-reo.herokuapp.com/api/translate?word=' + englishWord.value);
+        ourRequest.onload = function() {
+            console.log(ourRequest.responseText);
+        };
+        ourRequest.send();
+
+    }
 
 
 }
