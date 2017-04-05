2577dfad3b7b18a1026dc2d0ab2e5f6f713b443a
Alisa Yunusova <alisa.yunusova@gmail.com>
2017-04-05 16:26:11 +1200

    Refactoring: get rid of global names

--- tereo.js
+++ tereo.js
@@ -1,18 +1,19 @@
-document.addEventListener('DOMContentLoaded', startGame)
+document.addEventListener('DOMContentLoaded', () => {
 
-function startGame() {
-    var maoriWordContainer = document.getElementById("maoriWordContainer");
-    var button = document.getElementById("button");
-    var englishWord = document.getElementById("englishWord");
-    button.addEventListener("click", translateWord);
+      // Cache DOM elements
+      var maoriWordContainer = document.getElementById("maoriWordContainer");
+      var button = document.getElementById("button");
+      var englishWord = document.getElementById("englishWord");
 
-    function translateWord() {
-        var ourRequest = new XMLHttpRequest();
+      button.addEventListener("click", translateWord);
 
-        ourRequest.open('GET', 'https://eda-te-reo.herokuapp.com/api/translate?word=' + englishWord.value);
-        ourRequest.onload = () => {
-            maoriWordContainer.innerHTML = ourRequest.responseText;
-        };
-        ourRequest.send();
-    }
-}
+      function translateWord() {
+          var ourRequest = new XMLHttpRequest();
+
+          ourRequest.open('GET', 'https://eda-te-reo.herokuapp.com/api/translate?word=' + englishWord.value);
+          ourRequest.onload = () => {
+              maoriWordContainer.innerHTML = ourRequest.responseText;
+          };
+          ourRequest.send();
+      }
+});
